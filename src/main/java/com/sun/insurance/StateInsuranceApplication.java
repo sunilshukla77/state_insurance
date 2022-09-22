a9bbab08eada9e20e56b5b281b2d172cd2d3a7a3
sunilshukla77 <sunil.jobs77@gmail.com>
2022-09-22 08:08:58 +0530

    first Commit

---
+++ src/main/java/com/sun/insurance/StateInsuranceApplication.java
@@ -0,0 +1,13 @@
+package com.sun.insurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StateInsuranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StateInsuranceApplication.class, args);
+	}
+
+}
